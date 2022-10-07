a1db9560bd2000313d044fac9ed29bac5d5b5f62
nvarshney91 <varshneynishant0@gmail.com>
2022-10-07 22:21:35 +0530

    -- dark theme init --

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import Work from "./components/Work";
 
 import "./App.css";
 import resume from "./Resume.pdf";
+import { useState } from "react";
 
 function App() {
+  const [dark,setDark]=useState(false);
   return (
-    <div>
-      <header className="py-5">
+    <div className={dark?"bg-black h-screen":"bg-white h-screen"}>
+      <header className={dark?"text-white py-5":"py-5"}>
         <nav className="flex justify-between items-center">
           <Link to="/" className="font-extrabold text-lg tracking-wider">
             Av.11
@@ -23,7 +25,7 @@ function App() {
             <button className="py-1 px-3 cursor-pointer">
               <Link to="/work">Work</Link>
             </button>
-            <i class="py-1 px-3 cursor-pointer fa-solid fa-moon"></i>
+            <i onClick={(e)=>setDark(!dark)} class="py-1 px-3 cursor-pointer fa-solid fa-moon"></i>
           </div>
         </nav>
       </header>
